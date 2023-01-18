ad50533bf205daba4fb85d9434b29be7f68a6dbc
bemiyaz <benito.miya172@gmail.com>
2023-01-18 18:59:00 -0300

    implemente UserDetails interface in User class

---
+++ src/main/java/com/benitomiyazato/security/model/Role.java
@@ -0,0 +1,6 @@
+package com.benitomiyazato.security.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/benitomiyazato/security/model/User.java
+++ src/main/java/com/benitomiyazato/security/model/User.java
@@ -2,6 +2,12 @@ package com.benitomiyazato.security.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "user_table")
 @Entity
@@ -10,7 +16,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -23,4 +29,36 @@ public class User {
     private String email;
 
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
