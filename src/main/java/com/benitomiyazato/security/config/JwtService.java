4c1d1e3f4a52630aa31cd6bb30875054d0f785bb
bemiyaz <benito.miya172@gmail.com>
2023-01-18 20:06:25 -0300

    extractAllClaims and getSigningKey methods

--- src/main/java/com/benitomiyazato/security/config/JwtService.java
+++ src/main/java/com/benitomiyazato/security/config/JwtService.java
@@ -1,11 +1,33 @@
 package com.benitomiyazato.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "2D4B6150645367566B59703373367639792F423F4528482B4D6251655468576D";
+
     public String extractUsername(String token){
         return "baiano";
+    }
+
+    public Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
