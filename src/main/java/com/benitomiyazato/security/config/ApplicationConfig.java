740e2d3a29dbea7d7d9ba8ffd0d50ed4f9cc4612
bemiyaz <benito.miya172@gmail.com>
2023-01-23 16:57:53 -0300

    authentication manager beang

--- src/main/java/com/benitomiyazato/security/config/ApplicationConfig.java
+++ src/main/java/com/benitomiyazato/security/config/ApplicationConfig.java
@@ -4,8 +4,10 @@ import com.benitomiyazato.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,5 +36,10 @@ public class ApplicationConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder());]
 
         return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
